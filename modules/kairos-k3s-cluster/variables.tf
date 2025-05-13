39188997fbaa12ea918f6ebded4e2b1aa95d075d
Daniel Matthews <d.matthews@ucl.ac.uk>
2025-05-13 11:55:56 +0100

    Correct variable validation

--- modules/kairos-k3s-cluster/variables.tf
+++ modules/kairos-k3s-cluster/variables.tf
@@ -110,17 +110,16 @@ variable "ssh_ca_public_key" {
   type        = string
   description = "SSH CA public key to use for the VMs"
   default     = ""
+  validation {
+      condition     = var.ssh_ca_public_key != "" || var.ssh_public_key != ""
+      error_message = "Set one of ssh_ca_public_key or ssh_public_key"
+  }
 }
 
 variable "ssh_public_key" {
   type        = string
   description = "SSH public key to use for the VMs"
   default     = ""
-
-  validation {
-    condition     = var.ssh_ca_public_key != "" && var.ssh_public_key != ""
-    error_message = "Set one of ssh_ca_public_key or ssh_public_key"
-  }
 }
 
 variable "ssh_common_args" {
