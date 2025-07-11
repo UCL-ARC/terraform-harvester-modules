5096f05396d730103353dd35c4663ff316bd15aa
Daniel Matthews <d.matthews@ucl.ac.uk>
2025-07-11 21:21:47 +0100

    fix: Correct additional_bundles var definition (#29)

--- modules/kairos-k3s-cluster/variables.tf
+++ modules/kairos-k3s-cluster/variables.tf
@@ -1,7 +1,7 @@
 variable "additional_bundles" {
   type = list(object({
     target = string
-    values = optional(map(map(any)))
+    values = optional(map(any))
   }))
   default     = []
   description = "List of additional kairos community bundles to install in the cluster"
