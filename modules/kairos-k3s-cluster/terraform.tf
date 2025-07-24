7149711d6c1a0706df7e6ee49e48558a4d2d45aa
Daniel Matthews <d.matthews@ucl.ac.uk>
2025-07-24 12:34:05 +0100

    fix: Update providers and pre-commit hooks (#31)

--- modules/kairos-k3s-cluster/terraform.tf
+++ modules/kairos-k3s-cluster/terraform.tf
@@ -12,11 +12,6 @@ terraform {
       version = "3.7.2"
     }
 
-    remote = {
-      source  = "tenstad/remote"
-      version = "0.1.3"
-    }
-
     tls = {
       source  = "hashicorp/tls"
       version = "4.1.0"
